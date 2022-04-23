965711327dd3675c1193e3de138ea4e8c6ef9e41
Araxeus <78568641+Araxeus@users.noreply.github.com>
2022-04-24 02:43:59 +0300

    fix: fs/promises use

--- upgrade_version.js
+++ upgrade_version.js
@@ -2,10 +2,12 @@ import { readFile, writeFile } from 'fs/promises';
 
 import json from './package.json' assert { type: 'json' }; // { version as oldVersion }
 const oldVersion = json.version;
+let newVersion;
 
 let [, , type] = process.argv;
 const typeMap = ['major', 'minor', 'patch'];
 if (type === undefined) {
+    const titleCase = str => str[0].toUpperCase() + str.slice(1);
     const { red, yellow, green } = (await import('chalk')).default
     const colors = [red, yellow, green];
     const sign = '⭕';
@@ -15,13 +17,16 @@ if (type === undefined) {
                     type: 'list',
                     name: 'output',
                     message: 'What kind of upgrade would you like to do?',
-                    choices: typeMap.map((type, i) => `${colors[i](sign)} ${type} (${upgrade(oldVersion, i)})`).reverse()
+                    choices: [...typeMap.map((type, i) => `${colors[i](sign)} ${titleCase(type)} (${upgrade(oldVersion, i)})`).reverse(), `${sign} Cancel`]
                 }
-     ])).output;
+     ])).output.split(' ')[1];
+     if (type === 'Cancel') process.exit(0);
 }
 
-const index = isNaN(type) ? typeMap.indexOf(type) : Number(type);
+const index = isNaN(type) ? typeMap.indexOf(type.toLowerCase()) : Number(type);
 if (!typeMap[index]) throw 'invalid type';
+
+newVersion = upgrade(oldVersion, index);
 
 function upgrade(version, index) {
     const arr = version.split('.').map(Number);
@@ -34,31 +39,15 @@ function upgrade(version, index) {
 const regex = /(scriptVersion = '|version": ")(\d+\.\d+\.\d+)/;
 const replacer = (_, line, version) => line + upgrade(version, index);
 
-function upgradeInFile(path) {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf8', (readErr, data) => {
-            if (readErr) {
-                reject(readErr);
-            } else {
-                writeFile(
-                    path,
-                    data.replace(regex, replacer),
-                    'utf8',
-                    writeErr => {
-                        if (writeErr) reject(writeErr);
-                        resolve();
-                    }
-                );
-            }
-        });
-    });
+async function upgradeInFile(path) {
+    const content = await readFile(path, 'utf8');
+    const newContent = content.replace(regex, replacer);
+    if (newContent !== content) await writeFile(path, newContent);
 }
 
 function upgradeInFiles(...paths) {
     return Promise.all(paths.map(upgradeInFile));
 }
-
-let newVersion;
 
 upgradeInFiles('./index.js', './package.json')
     .then(() => {
